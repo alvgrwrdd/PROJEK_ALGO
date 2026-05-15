30745c28d9096a01773501fe5ea316a53464a0ad
tsabitar <tsabitarrosyid@gmail.com>
2026-05-15 22:14:12 +0700

    inisialisasi fungsi string kustom
    Menambahkan utilitas my_strcpy, my_strcmp, dan my_strlen.

--- projek.cpp
+++ projek.cpp
@@ -1,2 +1,19 @@
 #include <iostream>
 #include <stdio.h>
+using namespace std;
+
+void my_strcpy(char* dest, const char* src) {
+    while (*src) { *dest = *src; dest++; src++; }
+    *dest = '\0';
+}
+
+bool my_strcmp(const char* s1, const char* s2) {
+    while (*s1 && *s1 == *s2) { s1++; s2++; }
+    return (*s1 == *s2);
+}
+
+int my_strlen(const char* s) {
+    int len = 0;
+    while (*s) { len++; s++; }
+    return len;
+}
